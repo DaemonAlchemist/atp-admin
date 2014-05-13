20ea27eed9483064a6c142ae925e0f2f8a4877fc
Andrew Wittrock <andy@wittrock.us>
2014-05-13 00:56:19 -0500

    Make admin user view filter match the base class signature

--- src/ATPAdmin/View/Filter/AdminUser.php
+++ src/ATPAdmin/View/Filter/AdminUser.php
@@ -4,7 +4,7 @@ namespace ATPAdmin\View\Filter;
 
 class AdminUser extends \ATPCore\View\Filter\AbstractBlockFilter
 {
-	protected function _replace()
+	protected function _replace($block)
 	{
 		return \ATPAdmin\Auth::currentUser()->username;
 	}
